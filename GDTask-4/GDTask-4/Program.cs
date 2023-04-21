34463714a8843490e776e1846e3eb9738f2bcba5
Lucky932 <andersandrenatosamp@mail.ru>
2023-04-21 17:58:32 +0300

    GDTask-4/

---
+++ GDTask-4/GDTask-4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите код символа: ");
+
+int code = Convert.ToInt32(Console.ReadLine());
+
+void decoder(int code)
+{
+    if (code > 255)
+    {
+        code = code - 256;
+        Console.Write($"Символ в соответствии с кодом {code} : {Convert.ToChar(code)}");
+    }
+    else if (code < 0)
+    {
+        Console.WriteLine("Код не может быть меньше нуля. Повторите попытку.");
+    }
+    else
+    {
+        Console.Write($"Символ в соответствии с кодом {code} : {Convert.ToChar(code)}");
+    }
+    
+}
+
+decoder(code);
